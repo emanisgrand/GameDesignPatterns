5e601d783c1c9d1794bc6f86200ecd4e1b7cffd6
eman <tenhitcombo@gmail.com>
2019-08-19 12:41:19 -0400

    command script

---
+++ Assets/Command.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Command 
+{
+    //? I've never seen this new way of creating a reference.
+    //? In this case we've created the Animator reference within the method itself. cool
+    public abstract void Execute(Animator anim);
+}
+
+public class PerformJump: Command {
+    public override void Execute(Animator anim){
+        anim.SetTrigger("isJumping");
+    }
+}
+
+public class DoNothing:Command {
+    public override void Execute(Animator anim){
+        
+    }
+}
