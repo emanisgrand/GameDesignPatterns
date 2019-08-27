57d5a3b885efd49976e444a6f722b75a38c8e368
Eman Martinez <tenhitcombo@gmail.com>
2019-08-27 12:27:18 -0400

    created plantData for Plant.cs & attached to all tree prefabs

---
+++ Assets/Scripts/Plant.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour
+{
+    [SerializeField]
+    // pass the scriptable object through to the Plant.cs
+    private PlantData info;
+}
---
+++ Assets/Scripts/PlantData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "plantData", menuName = "Plant Data", order = 51)]
+public class PlantData : ScriptableObject
+{
+    public enum THREAT {None, Low, moderate, High}
+    
+    [SerializeField]
+    private string plantName;
+    [SerializeField]
+    private THREAT plantThreat;
+    [SerializeField]
+    private Texture icon;
+
+}
